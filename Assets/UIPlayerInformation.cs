4ea1531f05e0261585de534032c335d5bf6fba1f
Joseph Hesketh <joe.m.hesketh@gmail.com>
2017-11-09 13:03:38 +0000

    Nicer UI

--- Assets/UIPlayerInformation.cs
+++ Assets/UIPlayerInformation.cs
@@ -11,33 +11,41 @@ public class UIPlayerInformation : MonoBehaviour {
     public Image upButton;
     public Image rightButton;
     public Image downButton;
-	
+
 	// Update is called once per frame
 	void Update () {
         if (player.leftInventorySlot != null) {
             leftButton.sprite = player.leftInventorySlot.uiSprite;
+            leftButton.enabled = true;
         } else {
             leftButton.sprite = null;
+            leftButton.enabled = false;
         }
 
         if (player.upInventorySlot != null) {
             upButton.sprite = player.upInventorySlot.uiSprite;
+            upButton.enabled = true;
         } else {
             upButton.sprite = null;
+            upButton.enabled = false;
         }
 
 
         if (player.rightInventorySlot != null) {
             rightButton.sprite = player.rightInventorySlot.uiSprite;
+            rightButton.enabled = true;
         } else {
             rightButton.sprite = null;
+            rightButton.enabled = false;
         }
 
 
         if (player.downInventorySlot != null) {
             downButton.sprite = player.downInventorySlot.uiSprite;
+            downButton.enabled = true;
         } else {
             downButton.sprite = null;
+            downButton.enabled = false;
         }
 
     }
