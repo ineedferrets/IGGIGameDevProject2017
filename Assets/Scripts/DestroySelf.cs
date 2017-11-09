98fc237d17113843876373840ad66b343653d07a
Castaras <erv503@york.ac.uk>
2017-11-09 17:19:25 +0000

    Potions - Explosion and Wall
    Very hacky Wall potion works, explosion works.
    
    TODO: Put them into main game.

--- Assets/Scripts/DestroySelf.cs
+++ Assets/Scripts/DestroySelf.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DestroySelf : MonoBehaviour {
 
     public float Delay = 3f; // Delay in seconds before destroying the explosion particle
-	public Transform floorPrefab;
 	
     void Start() {
         Destroy(gameObject, Delay);
---
+++ Assets/Scripts/Potions/wallBuild.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallBuild : MonoBehaviour {
+
+	public Transform obstaclePrefab; 
+	private bool obstaclePlaced = false;
+	
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Build" && obstaclePlaced == false) {
+			obstaclePlaced = true;
+			Transform newObstacle = Instantiate(obstaclePrefab, transform.position, Quaternion.identity) as Transform; 
+		}
+	}
+}
