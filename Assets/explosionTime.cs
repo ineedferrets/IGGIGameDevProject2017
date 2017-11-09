b563fcbdde00d05313c2ac7000de93d970815856
Castaras <erv503@york.ac.uk>
2017-11-09 15:25:41 +0000

    healthbar sprites added

---
+++ Assets/explosionTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosionTime : MonoBehaviour {
+
+	private Transform[] obstacle;
+	// Use this for initialization
+	void Start () {
+		Invoke("Explosion", 5f);
+	}
+	
+	void Explosion () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		
+	}
+}
