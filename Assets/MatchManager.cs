90ebbdd7b339310e441c5474b43dee3906765a0b
Joseph Hesketh <joe.m.hesketh@gmail.com>
2017-11-10 10:44:40 +0000

    Added end game screen

--- Assets/MatchManager.cs
+++ Assets/MatchManager.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MatchManager : MonoBehaviour {
 
     public List<PlayerController> allPlayers;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Text winText;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (allPlayers.Count == 1) {
+            winText.text = "Game Over! " + allPlayers[0].name + " wins!";
+            winText.enabled = true;
+        }
 	}
 }
